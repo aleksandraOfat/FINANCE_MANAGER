32d1c459fd948c94f61cfa32ab7b41b28b70af95
aleksandraOfat <s27683@pjwstk.edu.pl>
2024-06-27 18:49:16 +0200

    Add files via upload

---
+++ create_database.py
@@ -0,0 +1,75 @@
+import sqlite3
+
+conn = sqlite3.connect('finance.db')
+c = conn.cursor()
+
+# Utworzenie tabeli wydatków
+c.execute('''CREATE TABLE IF NOT EXISTS expenses
+             (id INTEGER PRIMARY KEY,
+              amount REAL,
+              category TEXT,
+              month INTEGER,
+              year INTEGER)''')
+
+# Utworzenie tabeli przychodów
+c.execute('''CREATE TABLE IF NOT EXISTS incomes
+             (id INTEGER PRIMARY KEY,
+              amount REAL,
+              month INTEGER,
+              year INTEGER)''')
+
+# Utworzenie tabeli celów oszczędnościowych
+c.execute('''CREATE TABLE IF NOT EXISTS savings_goals
+             (id INTEGER PRIMARY KEY,
+              name TEXT,
+              target_amount REAL,
+              saved_amount REAL)''')
+
+# Utworzenie tabeli wpłat na cele oszczędnościowe
+c.execute('''CREATE TABLE IF NOT EXISTS savings_contributions
+             (id INTEGER PRIMARY KEY,
+              goal_id INTEGER,
+              amount REAL,
+              date TEXT,
+              FOREIGN KEY(goal_id) REFERENCES savings_goals(id))''')
+
+# Zamknięcie połączenia z bazą danych
+conn.commit()
+conn.close()
+
+def delete_all_expenses():
+    conn = sqlite3.connect('finance.db')
+    c = conn.cursor()
+    c.execute("DELETE FROM expenses")
+    conn.commit()
+    conn.close()
+
+def delete_all_incomes():
+    conn = sqlite3.connect('finance.db')
+    c = conn.cursor()
+    c.execute("DELETE FROM incomes")
+    conn.commit()
+    conn.close()
+
+# Usuwanie wszystkich wydatków z tabeli expenses
+# delete_all_expenses()
+# delete_all_incomes()
+
+
+def delete_all_savings():
+    conn = sqlite3.connect('finance.db')
+    c = conn.cursor()
+    c.execute("DELETE FROM savings_goals")
+    conn.commit()
+    conn.close()
+
+def delete_all_contributions():
+    conn = sqlite3.connect('finance.db')
+    c = conn.cursor()
+    c.execute("DELETE FROM savings_contributions")
+    conn.commit()
+    conn.close()
+# delete_all_savings()
+# delete_all_contributions()
+
+
